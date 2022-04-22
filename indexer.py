4a0952e956f6a2556ee363212a41b15975e15a01
MuhiimAli <muhiim_ali@brown.edu>
2022-04-21 20:52:28 -0400

    Sve

--- indexer.py
+++ indexer.py
@@ -32,7 +32,6 @@ class Indexer:
         
     docs_to_words_to_counts = {}  
     def parse(self):
-        count = 0
         for page in self.all_pages:#looping through all the pages
             text: str = page.find('text').text #getting the text of each page (as a str)
             id: int = int(page.find('id').text)
@@ -43,33 +42,16 @@ class Indexer:
                     sliced_links= self.handle_Links(term)
                     sliced_links_token = re.findall(self.tokenization_regex, sliced_links)#tokenizes link texts
                     for word in sliced_links_token:
-
                         word_stem = self.remove_stop_words_and_stem(word)
+                        self.populate_word_to_ids_counts_dict(word_stem, id)
                 else: #if the word is not a link
                     word_stem= self.remove_stop_words_and_stem(term)
-                if word_stem != None:
-                        if word_stem not in self.docs_to_words_to_counts:
-                            self.docs_to_words_to_counts[word_stem] = {}
-                        if id not in self.docs_to_words_to_counts[word_stem]:
-                            self.docs_to_words_to_counts[word_stem][id] = 0
-                        self.docs_to_words_to_counts[word_stem][id]+=1
-
-
-                        # if page not in self.docs_to_words_to_counts[word_stem]:
-                        #     self.docs_to_words_to_counts[word_stem][page] = 0
-                        
-                            # inner_dict = self.docs_to_words_to_counts[word_stem]
-                            # inner_dict[page]
-                        #self.word_corpus.add(word_stem)
-                        
-                
-           
-
+                    self.populate_word_to_ids_counts_dict(word_stem, id)
                # print(term)
                 
         #print(self.word_corpus)
+
         print(self.docs_to_words_to_counts)
-        """ treat linked text as words as well so that we can stem the linked test/remove stop/update maps """
 
     def handle_Links(self, term : str):
         if "|" in term:
@@ -85,12 +67,6 @@ class Indexer:
             processed_word =nltk_test.stem(term)
             self.word_corpus.add(processed_word)
             return processed_word
-            #print(processed_word)
-            # if processed_word != None:
-
-            # return processed_word
-           # 
-
     def ids_to_titles(self):
         ids_to_titles_dict = {}
         for page in self.all_pages:
@@ -98,7 +74,16 @@ class Indexer:
             id: int = int(page.find('id').text)
             ids_to_titles_dict[id] = title
             self.file_io.write_title_file(self.title_file, ids_to_titles_dict)
-        print(ids_to_titles_dict)
+
+    def populate_word_to_ids_counts_dict(self, word_stem: str, id : int):
+        if word_stem != None:
+            if word_stem not in self.docs_to_words_to_counts:
+                self.docs_to_words_to_counts[word_stem] = {}
+            if id not in self.docs_to_words_to_counts[word_stem]:
+                self.docs_to_words_to_counts[word_stem][id] = 0
+            self.docs_to_words_to_counts[word_stem][id]+=1
+
+
     def term_frequency():
         pass
         
